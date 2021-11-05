429efdf0ff40d71096282bf7ec561631afb5bab1
smweldes <weldesiem@gmail.com>
2021-11-05 02:02:21 -0700

    updated forminput

--- src/App.js
+++ src/App.js
@@ -3,67 +3,21 @@ import { useState, useRef } from 'react';
 import FormInput from "./components/FormInput";
 
 function App() {
-  const [value, setValue] = useState({
-    username:"",
-    email:"",
-    birthday:"",
-    password:"",
-    confirmPassword:"",
-  });
-
-  const inputs = [
-    {
-    id:1,
-    name:"username",
-    type:"text",
-    placeholder:"Username",
-    label: "Username"
-    },
-    {
-      id:2,
-      name:"email",
-      type:"text",
-      placeholder:"Email",
-      label: "Email"
-      },
-      {
-        id:3,
-        name:"birthday",
-        type:"text",
-        placeholder:"birthday",
-        label: "Birthday"
-        },
-        {
-          id:4,
-          name:"password",
-          type:"text",
-          placeholder:"Password",
-          label: "Password"
-          },
-          {
-            id:5,
-            name:"confirmPassword",
-            type:"text",
-            placeholder:"Confirm Password",
-            label: "Confirm Password"
-            }
-  ]
-  
+  const [username, setUsername] = useState("");
+  const usernameRef = useRef()
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target)
-    console.log(Object)
-  };
+  }
 
   return (
     <div className="app">
        
        <form onSubmit={handleSubmit}>
-         {inputs.map((input) => (
-           <FormInput name="username" {...input}/>
-         ))}
-         
+         <FormInput refer={usernameRef} placeholder="Username" setUsername/>
+         <FormInput placeholder="Email"/>
+         <FormInput placeholder="Full Name"/>
+         <FormInput placeholder="Sth else"/>
          <button>Submit</button>
        </form>
     </div>
---
+++ src/App1.js
@@ -0,0 +1,73 @@
+import './App.css';
+import { useState, useRef } from 'react';
+import FormInput from "./components/FormInput";
+
+function App() {
+  const [value, setValue] = useState({
+    username:"",
+    email:"",
+    birthday:"",
+    password:"",
+    confirmPassword:"",
+  });
+
+  const inputs = [
+    {
+    id:1,
+    name:"username",
+    type:"text",
+    placeholder:"Username",
+    label: "Username"
+    },
+    {
+      id:2,
+      name:"email",
+      type:"text",
+      placeholder:"Email",
+      label: "Email"
+      },
+      {
+        id:3,
+        name:"birthday",
+        type:"text",
+        placeholder:"birthday",
+        label: "Birthday"
+        },
+        {
+          id:4,
+          name:"password",
+          type:"text",
+          placeholder:"Password",
+          label: "Password"
+          },
+          {
+            id:5,
+            name:"confirmPassword",
+            type:"text",
+            placeholder:"Confirm Password",
+            label: "Confirm Password"
+            }
+  ]
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target)
+    console.log(Object)
+  };
+
+  return (
+    <div className="app">
+       
+       <form onSubmit={handleSubmit}>
+         {inputs.map((input) => (
+           <FormInput name="username"  placeholder="Username" setUsername/>
+         ))}
+         
+         <button>Submit</button>
+       </form>
+    </div>
+  );
+}
+
+export default App;
